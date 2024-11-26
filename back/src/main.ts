4c957514970fd51e0beed1fde00fb286ddc232ed
Ferreira-Orlann <118378969+Sora-u@users.noreply.github.com>
2024-11-26 12:14:21 +0100

    add 404

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NotFoundExceptionFilter } from './exception-filters/not-found';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.setGlobalPrefix("api")
-	await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalFilters(new NotFoundExceptionFilter());
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
