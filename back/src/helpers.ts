34abc89505717943583fb2b27cc71d24a45f2391
Ferreira-Orlann <118378969+Sora-u@users.noreply.github.com>
2024-11-27 14:54:36 +0100

    add protection to ApplyDtoToModel

--- back/src/helpers.ts
+++ back/src/helpers.ts
@@ -1,5 +1,8 @@
 export function ApplyDtoToModel(dto, model) {
     Object.keys(dto).forEach(function (key) {
+        if (key.endsWith("_id")) {
+            return
+        }
         model[key] = dto[key];
     });
 }
